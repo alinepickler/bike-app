f0bb4f76ade474e78088ea50c4a5198dcebee9f7
Aline Pickler <aline.pickler@gmail.com>
2017-11-17 21:43:19 +0100

    Change prices and add black-bike image

--- app/components/BikeApp.js
+++ app/components/BikeApp.js
@@ -14,12 +14,13 @@ export default class BikeApp extends React.Component {
 var ICON = '../images/bike-marker.png';
 var STOCKHOLM = {lat: 59.3293, lng: 18.0686};
 var DESCRIPTION = "Aenean ultrices nibh id facilisis suscipit. Aliquam mauris enim, tempus ac cursus et, consequat quis enim. Sed eget lectus euismod est molestie dapibus sit amet vel ante. Ut eget ornare metus. Duis eget lectus eu odio elementum lobortis. Etiam hendrerit urna at ultricies commodo.";
-var IMAGE = '../images/blue.png';
+var BLUE_BIKE_IMAGE = '../images/blue-bike.jpg';
+var BLACK_BIKE_IMAGE = '../images/black-bike.jpg';
 var BIKES = [
-  {lat: 59.3630, lng: 18.1468, price: '500,00kr', image: IMAGE, description: DESCRIPTION},
-  {lat: 59.3050, lng: 18.1088, price: '500,00kr', image: IMAGE, description: DESCRIPTION},
-  {lat: 59.3312, lng: 18.0407, price: '500,00kr', image: IMAGE, description: DESCRIPTION},
-  {lat: 59.3689, lng: 18.0084, price: '500,00kr', image: IMAGE, description: DESCRIPTION},
-  {lat: 59.3315, lng: 18.0715, price: '500,00kr', image: IMAGE, description: DESCRIPTION},
-  {lat: 59.3118, lng: 18.0663, price: '500,00kr', image: IMAGE, description: DESCRIPTION}
+  {lat: 59.3630, lng: 18.1468, price: '200,00kr', image: BLUE_BIKE_IMAGE, description: DESCRIPTION},
+  {lat: 59.3050, lng: 18.1088, price: '500,00kr', image: BLACK_BIKE_IMAGE, description: DESCRIPTION},
+  {lat: 59.3312, lng: 18.0407, price: '350,00kr', image: BLUE_BIKE_IMAGE, description: DESCRIPTION},
+  {lat: 59.3689, lng: 18.0084, price: '400,00kr', image: BLACK_BIKE_IMAGE, description: DESCRIPTION},
+  {lat: 59.3315, lng: 18.0715, price: '210,00kr', image: BLUE_BIKE_IMAGE, description: DESCRIPTION},
+  {lat: 59.3118, lng: 18.0663, price: '150,00kr', image: BLACK_BIKE_IMAGE, description: DESCRIPTION}
 ];
